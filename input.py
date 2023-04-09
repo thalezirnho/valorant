653006ae5b520ad7675cc3aae69e26a248e45eca
Thales <thalesfollador@gmail.com>
2023-04-09 00:59:22 -0300

    Update input.py

--- input.py
+++ input.py
@@ -174,4 +174,4 @@ df_matches.to_gbq(
 
 
 blob = bucket.blob(f'valorant/logs/input_log_{exec_time}.txt')
-blob.upload_from_filename(file_name)
+blob.upload_from_filename(f'input_log_{exec_time}.txt')
