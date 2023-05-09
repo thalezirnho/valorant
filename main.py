c24cae741db5cf52a19c37bd92f74cd572e89029
Thales <thalesfollador@gmail.com>
2023-05-09 09:53:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-# Setup
-print('Starting main script...')
-print('Importing functions...')
+# TO DO
 
-from layer_input import valorant_layer_input
-from layer_tabular import valorant_layer_tabular
-
-
-# Call input
-print('Starting input...')
-session_matchs = valorant_layer_input()
-
-print(f'{len(session_session_matchs)} tables from input...')
-# Call tabular (only if we have new matchids)
-if len(session_matchs) >= 1:
-    # Run Tabular
-    print(f'{len(session_session_matchs)} tables will be processed...')
-    valorant_layer_tabular(matches=session_matchs)
-    print('End of tabular script...')
+## Setup
+#print('Starting main script...')
+#print('Importing functions...')
+#
+#from layer_input import valorant_layer_input
+#from layer_tabular import valorant_layer_tabular
+#
+#
+## Call input
+#print('Starting input...')
+#session_matchs = valorant_layer_input()
+#
+#print(f'{len(session_session_matchs)} tables from input...')
+## Call tabular (only if we have new matchids)
+#if len(session_matchs) >= 1:
+#    # Run Tabular
+#    print(f'{len(session_session_matchs)} tables will be processed...')
+#    valorant_layer_tabular(matches=session_matchs)
+#    print('End of tabular script...')
